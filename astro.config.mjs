44c2df533557e3350402a86a59acc2ee032cd1a4
EmiliBL <emla0003@stud.kea.dk>
2025-05-31 09:20:48 +0200

    please virk med ny kode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,13 @@
-// import { defineConfig } from "astro/config";
-// // import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'; 
 
-// import alpinejs from "@astrojs/alpinejs";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   // vite: {
-//   //   plugins: [tailwindcss()],
-//   // },
-//   integrations: [alpinejs()],
-// });
-
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  vite: {  
+    plugins: [tailwindcss()],
+  },
+  integrations: [alpinejs()],
 });
+
